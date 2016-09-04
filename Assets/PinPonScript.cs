fe796cbe0c40760cbf505dab8f9cc39272ca490f
Arthur <le-tutur@hotmail.fr>
2016-09-04 13:11:18 +0200

    PinPon random volum & protester mesh ok but rolling

---
+++ Assets/PinPonScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinPonScript : MonoBehaviour {
+
+	private bool wait = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!wait) {
+			wait = true;
+			StartCoroutine(RandomVolum());
+		}	
+	}
+
+	IEnumerator RandomVolum() {
+		gameObject.GetComponent<AudioSource>().volume = (Random.Range(0f, 0.12f));
+		yield return new WaitForSeconds(2f);
+		wait = false;
+	}
+}
